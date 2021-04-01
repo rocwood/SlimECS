e0f57ab6bc2197bbef22157498b0e2c758dad60c
rocwood <rocwood@flu3d.com>
2021-04-01 18:07:43 +0800

    code cleanup

--- Source/SlimECS/src/Entity/Entity.cs
+++ Source/SlimECS/src/Entity/Entity.cs
@@ -1,40 +1,32 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace SlimECS
 {
-    public struct Entity : IEquatable<Entity>
-    {
+	public struct Entity : IEquatable<Entity>
+	{
 		public static readonly Entity Empty = new Entity();
 
 		public readonly int id;
 		internal readonly int slot;
 
 		internal Entity(int id, int slot)
-        {
-            this.id = id;
-            this.slot = slot;
-        }
-
-        public override bool Equals(object obj) => obj is Entity entity && Equals(entity);
-        public bool Equals(Entity other) => id == other.id && slot == other.slot;
-		public override int GetHashCode() 
-        {
-			// https://referencesource.microsoft.com/#System.Numerics/System/Numerics/HashCodeHelper.cs
-			return ((id << 5) + id) ^ slot;
-
-			// https://programmingpraxis.com/2018/06/19/fibonacci-hash/
-			//return (int)(id * 2654435769L + slot);
-
-			/*
-			// automatically generated hash
-			var hashCode = -528825007;
-            hashCode = hashCode * -1521134295 + id;
-            hashCode = hashCode * -1521134295 + slot;
-            return hashCode;
-			*/
+		{
+			this.id = id;
+			this.slot = slot;
 		}
 
-		public static bool operator ==(Entity left, Entity right) => left.Equals(right);
-        public static bool operator !=(Entity left, Entity right) => !left.Equals(right);
-    }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)] 
+		public override bool Equals(object obj) => obj is Entity entity && Equals(entity);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)] 
+		public bool Equals(Entity other) => id == other.id && slot == other.slot;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)] 
+		public static bool operator ==(Entity left, Entity right) => left.id == right.id && left.slot == right.slot;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)] 
+		public static bool operator !=(Entity left, Entity right) => left.id != right.id || left.slot != right.slot;
+
+		// https://referencesource.microsoft.com/#System.Numerics/System/Numerics/HashCodeHelper.cs
+		public override int GetHashCode() => ((id << 5) + id) ^ slot;
+	}
 }
